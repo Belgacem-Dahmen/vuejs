c070ffe140ef7787bc7a4344b19a0e72c1116f5e
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2023-08-08 15:07:39 +0200

    vue-init

---
+++ testa/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <button @click="toogleShowModal" > ADD TICKET</button>
+  <div v-if="showModal">
+    <modal :title=title :content=content @close="closeModal" />
+  </div>
+  <h1> {{ user.title }}</h1>
+  <p>{{ user.nom }}</p>
+  <button @click="toogleShowRole">show Role</button>
+  <div v-if="showRole">
+    <p> {{ user.role }}</p>
+  </div>
+
+  <div>
+    <ul>
+      <li v-for="ticket in filteredTickets ">
+
+        <h3>{{ ticket.titre }}</h3>
+        <p> {{ ticket.description }}</p>
+        <button> edit </button>
+        <button> delete </button>
+      </li>
+
+    </ul>
+
+  </div>
+</template>
+
+
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showRole: true,
+      showModal: false,
+      title: "add a Ticket",
+      content:"Please fill the needed inputs",
+
+      user: {
+        title: "Testa",
+        nom: "kacem",
+        role: "user"
+      },
+      tickets: [
+        { id: 1, titre: "travaux", type: "inscription", description: "lorem10" },
+        { id: 2, titre: "inscription", type: "inscription", description: "lorem11" },
+        { id: 3, titre: "use case", type: "inscription", description: "lorem12" },
+        { id: 4, titre: "jeux de données ", type: "integration", description: "lorem13" },
+        { id: 3, titre: "use case 2 ", type: "inscription", description: "lorem14" },
+
+      ]
+
+    }
+  },
+  methods: {
+    toogleShowRole() {
+      this.showRole = !this.showRole
+    },
+    toogleShowModal() {
+      this.showModal = !this.showModal
+    },
+    closeModal (){
+      this.showModal = false
+    }
+  },
+  computed: {
+    filteredTickets() {
+      return this.tickets.filter((ticket) => ticket.type === "inscription")
+    }
+  }
+
+}
+
+
+
+
+</script>
+
+
+
+
---
+++ testa/src/components/Modal.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <h1>{{ title }}</h1>
+            <p>{{ content }}</p>
+            <div class="form">
+                <div class="form-inputs">
+                    <input type="text" placeholder="name">
+                    <input type="text" placeholder="type">
+                    <input type="text" placeholder="description">
+                </div>
+
+                <div><button> SAVE </button></div>
+            </div>
+            <button @click="closeModal" class="closeModal"> close</button>
+        </div>
+
+    </div>
+</template>
+  
+<style >
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal h1 {
+    color: red;
+}
+
+.modal p {
+    color: gray;
+}
+.modal .form{
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+}
+.modal .form-inputs {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    border-radius: 20px;
+}
+</style>
+  
+<script>
+export default {
+    props: [
+        'content',
+        'title'
+    ],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+
+
+}
+</script>
+  
+  
+  
+  
+  
